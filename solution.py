ee5be35b6e37923a00545231fceba5c3ada49611
panter-x <129738391+panter-x@users.noreply.github.com>
2023-04-11 21:43:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 #from scipy.stats import anderson_ksamp
-from scipy.stats import ks_2samp 
+#from scipy.stats import ks_2samp 
+from scipy import stats
 
 chat_id = 46951859 # Ваш chat ID, не меняйте название переменной
 
@@ -9,5 +10,6 @@ def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.01
     
     #return anderson_ksamp([x, y]).pvalue < alpha
-    pv = ks_2samp(x, y)[1]
+    #pv = ks_2samp(x, y)[1]
+    pv = stats.cramervonmises_2samp(x, y).pvalue
     return pv < alpha
