d614576321269e8aabd12eaca294e6ca4e19a8c2
panter-x <129738391+panter-x@users.noreply.github.com>
2023-04-12 21:27:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 #from scipy.stats import anderson_ksamp
 #from scipy.stats import ks_2samp 
-from scipy import stats
+#from scipy import stats
+from hyppo.ksample import Energy, MMD
 
 chat_id = 46951859 # Ваш chat ID, не меняйте название переменной
 
@@ -11,5 +12,7 @@ def solution(x: np.array, y: np.array) -> bool:
     
     #return anderson_ksamp([x, y]).pvalue < alpha
     #pv = ks_2samp(x, y)[1]
-    pv = stats.cramervonmises_2samp(x, y).pvalue
+    #pv = stats.cramervonmises_2samp(x, y).pvalue
+    pv = MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+
     return pv < alpha
